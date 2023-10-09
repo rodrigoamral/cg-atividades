8ab2f4fd1cc44b27b13af8c1b68350fc49304632
Rodrigo Amaral <98924912+rodrigoamral@users.noreply.github.com>
2023-10-09 17:34:19 -0300

    commit aitivdade 1

---
+++ atividade 1/documentacao/_build/html/_sources/index.rst.txt
@@ -0,0 +1,24 @@
+.. Atividade1 documentation master file, created by
+   sphinx-quickstart on Mon Oct  9 17:16:07 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Atividade1's documentation!
+======================================
+
+.. automodule:: atividade1
+   :members:
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ atividade 1/documentacao/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Atividade1'
+copyright = '2023, Rodrigo Henrique'
+author = 'Rodrigo Henrique'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'pt_br'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
